0a373d40ebfbcfc94f8d4d4a24a3753246aef35b
kakum3 <kakum_3@hotmail.com>
2022-06-29 19:13:18 +0000

    Añado Form

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const[newItem, setNewItem] = useState("");
+	const[items, setItems] = useState([]);
+
+	const handleSubmit= (event)=> {
+		event.preventDefault()
+		if(newItem !=("")){	
+			const addItems = {
+				id: Math.floor(Math.random()*1000),
+				value: newItem
+			}
+			setItems( [...items,addItems]);
+			setNewItem("");
+	    }
+	
+    }
+	const deleteItem = (id) =>{
+	const filterItems =[...items].filter(( items )=> items.id !== id);
+	setItems(filterItems);
+	}
+		
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="Home">
+			<h1 className="todos">Todos</h1>
+			<div className="list-card">
+			<form onSubmit={handleSubmit}>
+				<input type="text"
+				placeholder = "add and item..."
+				value={newItem}
+				onChange={e=>setNewItem(e.target.value)}
+				/>
+				<button>Add</button>
+			</form>
+			<ul className="list">
+				{items.map(item=>{
+					return(
+						<li key={item.id}>{item.value}<button onClick={()=>deleteItem(item.id)}>X</button></li>
+					)
+				})}
+
+			</ul>
+			
+		</div>	
 		</div>
 	);
-};
+}
 
 export default Home;
